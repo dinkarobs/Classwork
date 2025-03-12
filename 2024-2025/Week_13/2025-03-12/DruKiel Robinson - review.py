8d6dedbbaa09968511f5c8e5a9360d08af9dd841
373778 <drobinson43@bostonk12.org>
2025-03-12 10:42:23 -0400

    Python(everything not turned in)

--- 2024-2025/Week_13/2025-03-12/DruKiel Robinson - review.py
+++ 2024-2025/Week_13/2025-03-12/DruKiel Robinson - review.py
@@ -1,19 +1,19 @@
 # Please leave 2 comments below, one with your name, and the other with
 # today's date
-
-
+#Drukiel Robinson
+#3/12/25
 
 
 
 # Create a variable called random_input and use it to get some text from the
 # user.
-
+random_input=input("Give me a random text")
 
 
 
 # If you want to find out what type a variable is in Python, what method do
 # you use?
-
+type(random_input)
 
 
 
@@ -21,42 +21,44 @@
 # Use the appropriate method to display the type of the random_input
 # variable. Put a comment below the code with the output you get from
 # running the code.
-
-
+print(type(random_input))
+#<class 'str'>
 
 
 # Please create a variable called name and get the name from the user and
 # store it in the variable.  The variable should be a string.
-
-
+name=input("What is your name?")
+#don't have to put str because already a string
 
 
 
 # Use Python to output proof that your variable name is a string
-
+print(type(name))
 
 
 
 # Please create a variable called age and get the name from the user and
 # store it in the variable.  The variable should be an integer.
-
+age=int(input("What is your age:"))
 
 
 
 # Use Python to output proof that your variable age is an integer
-
+print(type(age))
 
 
 
 # Please create a variable called gpa and get the gpa from the user and
 # store it in the variable.  The variable should be a float.
-
+gpa=float(input("What is your GPA:"))
 
 
 # Use Python to output proof that your variable gpa is a float
+print(type(gpa))
 
 
 
+#use python to output a one line complete sentence with your name, age and gpa
 
-# Use Python to output a one line complete sentence with your name, age and
-# GPA.
+print(name),print(age),print(gpa)
+
