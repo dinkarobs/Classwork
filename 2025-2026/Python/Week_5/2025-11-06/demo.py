9e9272052bdb8d93c11e8e8e6dba1c4bb2fdb035
373778 <drobinson43@bostonk12.org>
2025-11-06 10:51:10 -0500

    w

---
+++ 2025-2026/Python/Week_5/2025-11-06/demo.py
@@ -0,0 +1,33 @@
+#Drukiel Robinson
+#2025-11-06
+def main():
+    name="Drukiel"
+    age=16
+    print("Mr O looks like tung tung sahur")
+    brainrot = input("What is your favorite brainrot?")
+    print("your name is:", name)
+    print("you are", age, "years old")
+    print("your chose", brainrot)
+
+    num1=-67
+    num2=-67
+    num3=0
+    num4=0
+    num5=300
+    num6=300
+    num7=num1
+    num8=num4
+    num9=num6
+    num10=num5
+    print(id(num1))
+    print(id(num2))
+    print(id(num3))
+    print(id(num4))
+    print(id(num5))
+    print(id(num6))
+    print(id(num7))
+    print(id(num8))
+    print(id(num9))
+    print(id(num10))
+if __name__ == '__main__':
+    main()
