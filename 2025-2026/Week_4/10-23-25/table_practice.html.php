a60eab175115bbf4ad646aced2c79addda8aa713
373778 <drobinson43@bostonk12.org>
2025-12-18 11:32:01 -0500

    old work not commited

---
+++ 2025-2026/Week_4/10-23-25/table_practice.html.php
@@ -0,0 +1,34 @@
+<head>
+    <title>Tables</title>
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            margin-top: 20px;
+        }
+        td, th {
+            padding: 10px;
+        }
+    </style>
+
+</head>
+<body>
+<h1>Tables</h1>
+<table>
+    <tr>
+
+
+    </tr>
+
+
+
+
+
+</table>
+
+
+
+
+
+
+
+</body>
